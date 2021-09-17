191dde6f0a20e68a57addfcc148712697292c3bf
augusto cesar <acs-03@hotmail.com>
2021-09-16 21:44:42 -0300

    Post category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -30,5 +30,13 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encotrada!"));
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
 	}		
 }
