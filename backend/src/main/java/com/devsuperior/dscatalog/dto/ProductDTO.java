b83000dceefa610af6c0dec183e50eb9c4f89486
Augusto Cesar <acs-03@hotmail.com>
2021-10-06 22:35:26 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -14,10 +19,19 @@ public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+
+	@Size(min = 5, max = 60, message = "Campo deve ter entre 5 e 60 caracteres")
+    @NotBlank(message = "Campo obrigatório")
 	private String name;
+
+	@NotBlank(message = "Campo obrigatório")
 	private String description;	
+
+	@Positive(message = "Digite um valor positivo")
 	private Double price;
 	private String imgUrl;
+
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
@@ -27,7 +41,6 @@ public class ProductDTO implements Serializable{
 	}
 
 	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
@@ -12,8 +14,12 @@ public class UserDTO implements Serializable{
     private static final long serialVersionUID = 1L;
     
     private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Favor entrar com um e-mail válido")
     private String email;
 
     Set<RoleDTO> roles = new HashSet<>();
