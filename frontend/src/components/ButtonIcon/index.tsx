77c44ed23cbc3eea0545a9edca69643fff96d4d1
augusto cesar <acs-03@hotmail.com>
2021-11-16 22:58:11 -0300

    Home - part 3

---
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+    return (
+        <button className="btn btn-primary btn-icon">
+            <p>Inicie agora a sua busca</p>
+        </button>
+    )
+}
+
+export default ButtonIcon;
