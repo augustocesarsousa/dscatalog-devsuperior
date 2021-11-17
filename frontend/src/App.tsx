4f8308791237d1ce744ff4a45493ac4fe9bdf01b
augusto cesar <acs-03@hotmail.com>
2021-11-16 22:03:29 -0300

    Home - part 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import Navbar from './components/Navbar';
+import Home from 'pages/Home';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-       <>
-       <Navbar />
-       <h1>DSCatalog</h1>
-       </>
-       );       
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
