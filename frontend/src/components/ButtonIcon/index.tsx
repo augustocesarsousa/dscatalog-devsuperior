448a15c01d2946dfeb3cbf36237c36adb18750c8
augusto cesar <acs-03@hotmail.com>
2021-11-18 20:44:26 -0300

    Custom ButtonIcon

--- frontend/src/components/ButtonIcon/index.tsx
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,18 @@
 import './styles.css';
 
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <p>Inicie agora a sua busca</p>
-        </button>
-    )
-}
+  return (
+    <button className="btn btn-primary btn-container">
+      <div className="btn-text">
+        <p>Inicie agora a sua busca</p>
+      </div>
+      <div className="btn-icon">
+        <ArrowIcon />
+      </div>
+    </button>
+  );
+};
 
 export default ButtonIcon;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -13,9 +13,11 @@ const Home = () => {
             <h1>Conheça o melhor catálogo de produtos</h1>
             <p>Ajudaremos você a encontrar os melhores produtos no mercado</p>
           </div>
-          <Link to="/products">
-            <ButtonIcon />
-          </Link>
+          <div className="home-link-container">
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
