3b70bd764f645bed7d58f1f3e8808fa8158717c7
augusto cesar <acs-03@hotmail.com>
2021-11-24 15:41:22 -0300

    ProductDetails HTML

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,3 +1,32 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
 export default function ProductDetails() {
-  return <h1>Em construção</h1>;
+  return (
+    <div className="product-details-container">
+        <div className="product-details-card">
+            <div className="goback-container">
+                <ArrowIcon />
+                <p>Voltar</p>
+            </div>
+            <div className="row">
+                <div className="col-xl-6">
+                    <div className="img-container">
+                        <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Descrição da imagem" />
+                    </div>
+                    <div className="name-price-container">
+                        <h1>Nome do produto</h1>
+                        <ProductPrice price={1234.56}/>
+                    </div>
+                </div>
+                <div className="col-xl-6">
+                    <div className="description-container">
+                        <h2>Descrição do produto</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, hic.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
 }
