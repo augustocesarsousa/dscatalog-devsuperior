a7d76ce28aea779cf4d323c58bff64e2971cc60b
augusto cesar <acs-03@hotmail.com>
2021-11-24 19:52:25 -0300

    Catalog - ProductDetails links

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,5 +1,6 @@
 import ProductCard from 'components/ProductCard';
 import { Product } from 'types/product';
+import { Link } from 'react-router-dom';
 
 const Catalog = () => {
   const product: Product = {
@@ -27,19 +28,29 @@ const Catalog = () => {
     <div className="container my-4">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,34 +1,43 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 export default function ProductDetails() {
   return (
     <div className="product-details-container">
-        <div className="base-card product-details-card">
-            <div className="goback-container">
-                <ArrowIcon />
-                <p>Voltar</p>
+      <div className="base-card product-details-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <p>Voltar</p>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Descrição da imagem"
+              />
             </div>
-            <div className="row">
-                <div className="col-xl-6">
-                    <div className="img-container">
-                        <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Descrição da imagem" />
-                    </div>
-                    <div className="name-price-container">
-                        <h1>Nome do produto</h1>
-                        <ProductPrice price={1234.56}/>
-                    </div>
-                </div>
-                <div className="col-xl-6">
-                    <div className="description-container">
-                        <h2>Descrição do produto</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, hic.</p>
-                    </div>
-                </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={1234.56} />
             </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus, hic.
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    );
+  );
 }
