2d186c0d26de9beaf3412859ecffb8d7e4113121
augusto cesar <acs-03@hotmail.com>
2021-11-26 19:36:35 -0300

    Request pageable

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,31 +1,48 @@
 import ProductCard from 'components/ProductCard';
 import { Product } from 'types/product';
 import { Link } from 'react-router-dom';
+import Pagination from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import axios from 'axios';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
-import Pagination from 'components/Pagination';
 
 const Catalog = () => {
-  const product: Product = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params: AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12,
       },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
-  };
+    };
+
+    axios(params).then((response) => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
+
+  const product = {
+    "id": 1,
+    "name": "The Lord of the Rings",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "price": 90.5,
+    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+    "date": "2020-07-13T20:50:07.123450Z",
+    "categories": [
+        {
+            "id": 2,
+            "name": "Eletrônicos"
+        }
+    ]
+}
 
   return (
     <div className="container my-4 catalog-container">
