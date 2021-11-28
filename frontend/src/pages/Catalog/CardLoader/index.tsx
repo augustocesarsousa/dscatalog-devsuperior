7c1092f6c6e91d598e9b22a0454751ba5988932b
augusto cesar <acs-03@hotmail.com>
2021-11-27 21:52:08 -0300

    ProductDetails loaders

--- frontend/src/pages/Catalog/CardLoader/index.tsx
+++ frontend/src/pages/Catalog/CardLoader/index.tsx
@@ -1,18 +1,20 @@
 import ContentLoader from 'react-content-loader';
 
 const CardLoader = () => (
-  <ContentLoader
-    speed={2}
-    viewBox="0 0 300 450"
-    height={450}
-    width={300}
-    backgroundColor="#f3f3f3"
-    foregroundColor="#d6d2d2"
-  >
-    <rect x="30" y="20" rx="8" ry="8" width="250" height="200" />
-    <rect x="30" y="250" rx="0" ry="0" width="250" height="18" />
-    <rect x="30" y="275" rx="0" ry="0" width="120" height="20" />
-  </ContentLoader>
+  <div className="card-loader-container">
+    <ContentLoader
+      speed={2}
+      viewBox="0 0 300 450"
+      height={450}
+      width={300}
+      backgroundColor="#f3f3f3"
+      foregroundColor="#d6d2d2"
+    >
+      <rect x="30" y="20" rx="8" ry="8" width="250" height="200" />
+      <rect x="30" y="250" rx="0" ry="0" width="250" height="18" />
+      <rect x="30" y="275" rx="0" ry="0" width="120" height="20" />
+    </ContentLoader>
+  </div>
 );
 
 export default CardLoader;
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -5,23 +5,31 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
+import ProductDetailsLoader from './ProductDetailsLoader';
+import ProductInfoLoader from './ProductInfoLoader';
 
 import './styles.css';
 
 type UrlParams = {
   productId: string;
-}
+};
 
 export default function ProductDetails() {
-
   const { productId } = useParams<UrlParams>();
 
   const [product, setProduct] = useState<Product>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
-      setProduct(response.data);
-    });
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/products/${productId}`)
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [productId]);
 
   return (
@@ -35,19 +43,29 @@ export default function ProductDetails() {
         </Link>
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container">
-              <img src={product?.imgUrl} alt={product?.name} />
-            </div>
-            <div className="name-price-container">
-              <h1>{product?.name}</h1>
-              {product && <ProductPrice price={product?.price} />}
-            </div>
+            {isLoading ? (
+              <ProductInfoLoader />
+            ) : (
+              <>
+                <div className="img-container">
+                  <img src={product?.imgUrl} alt={product?.name} />
+                </div>
+                <div className="name-price-container">
+                  <h1>{product?.name}</h1>
+                  {product && <ProductPrice price={product?.price} />}
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="description-container">
-              <h2>Descrição do produto</h2>
-              <p>{product?.description}</p>
-            </div>
+            {isLoading ? (
+              <ProductDetailsLoader />
+            ) : (
+              <div className="description-container">
+                <h2>Descrição do produto</h2>
+                <p>{product?.description}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
