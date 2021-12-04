f7d5d8f6c4d7665f0f0fc3d88c92e382633a2c99
augusto cesar <acs-03@hotmail.com>
2021-12-03 22:06:02 -0300

    Auth Routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -21,6 +21,7 @@ const Routes = () => (
         <Route path="/products/:productId">
           <ProductDetails />
         </Route>
+        <Redirect from="/admin/auth" to="/admin/auth/login" exact />
         <Route path="/admin/auth">
           <Auth />
         </Route>
--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -1,5 +1,27 @@
+import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg'
+import { Switch, Route } from 'react-router-dom';
+
 export default function Auth () {
     return(
-        <h1>Autentication</h1>
+        <div className="auth-container">
+            <div className="auth-banner-container">
+                <h1>Divulgue seus produtos no DS Catalog</h1>
+                <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
+                <AuthImage />
+            </div>
+            <div className="auth-form-container">
+                <Switch>
+                    <Route path="/admin/auth/login">
+                        <h1>Card Login</h1>
+                    </Route>
+                    <Route path="/admin/auth/signup">
+                        <h1>Card Signup</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+                        <h1>Card Recover</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
     );
 }
