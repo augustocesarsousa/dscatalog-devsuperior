99693a96fdad5504f5bca872d13f1ad2fd7f03b4
augusto cesar <acs-03@hotmail.com>
2021-12-08 21:04:15 -0300

    check user authenticated

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -106,3 +106,8 @@ export const getTokenData = (): TokenData | undefined => {
     return undefined;
   }
 };
+
+export const isAuthenticated = (): boolean => {
+  const tokenData = getTokenData();
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
