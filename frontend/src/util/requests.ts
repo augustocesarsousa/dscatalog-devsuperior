799b831ef53a3c0d031ff839353102e63821a170
augusto cesar <acs-03@hotmail.com>
2021-12-09 21:27:54 -0300

    hasAnyRoles alternative high order function

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -121,13 +121,17 @@ export const hasAnyRoles = (roles: Role[]): boolean => {
 
   const tokenData = getTokenData();
 
-  if(tokenData !== undefined) {
-    for(let i = 0; i < roles.length; i++) {
-      if (tokenData.authorities.includes(roles[i])){
-        return true;
-      }
-    }
+  if (tokenData !== undefined) {
+    return roles.some(role => tokenData.authorities.includes(role));
   }
+
+  // if(tokenData !== undefined) {
+  //   for(let i = 0; i < roles.length; i++) {
+  //     if (tokenData.authorities.includes(roles[i])){
+  //       return true;
+  //     }
+  //   }
+  // }
 
   return false;
 };
