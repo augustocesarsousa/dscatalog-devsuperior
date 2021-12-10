37a326c646fb6e0059856abfa0207286fb8316b5
augusto cesar <acs-03@hotmail.com>
2021-12-09 21:47:58 -0300

    Route level permission

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -17,7 +17,7 @@ const Admin = () => {
                     <PrivateRoute path="/admin/categories">
                         <h1>Categorie CRUD</h1>
                     </PrivateRoute>
-                    <PrivateRoute path="/admin/users">
+                    <PrivateRoute path="/admin/users" roles={['ROLE_ADMIN']}>
                         <Users />
                     </PrivateRoute>
                 </Switch>
