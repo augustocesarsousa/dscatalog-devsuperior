0dcc46f7d70e462b0f26ffbfe71aea96c46bdc33
Augusto Cesar <acs-03@hotmail.com>
2021-12-28 17:27:05 -0300

    Starting layout product list

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,7 +1,33 @@
+import ProductCrudCard from "components/ProductCrudCard";
+
 const List = () => {
-    return(
-        <h1>Tela List</h1>
-    )
-}
+  const product = {
+    id: 1,
+    name: 'The Lord of the Rings',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 90.5,
+    imgUrl:
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
+    date: '2020-07-13T20:50:07.123450Z',
+    categories: [
+      {
+        id: 2,
+        name: 'Eletrônicos',
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <button className="btn btn-primary text-white">adicionar</button>
+      <div>Search bar</div>
+
+      <ProductCrudCard product={product}/>
+      <ProductCrudCard product={product}/>
+      <ProductCrudCard product={product}/>
+    </div>
+  );
+};
 
 export default List;
