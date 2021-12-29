9eb6cb30921bc239d4a7c129957cb20082ed8010
Augusto Cesar <acs-03@hotmail.com>
2021-12-28 22:03:58 -0300

    CategoryBadge

--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import ProductCrudCard from 'components/ProductCrudCard';
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import { Link } from 'react-router-dom';
 
 const List = () => {
@@ -10,12 +10,16 @@ const List = () => {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     price: 90.5,
     imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg',
+      'https://a-static.mlcdn.com.br/1500x1500/adesivo-80x58cm-poker-face-meme-ref-ade3052-nebula-decor/nebuladecor/2ae93b42966811eb824f4201ac1850e0/981aba7963fc15af115b7a313aa14e6d.jpg',
     date: '2020-07-13T20:50:07.123450Z',
     categories: [
       {
         id: 2,
         name: 'Eletrônicos',
+      },
+      {
+        id: 1,
+        name: 'Computadores',
       },
     ],
   };
