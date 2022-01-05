231971625d92e030dd5bcab87588e9678c216fe1
Augusto Cesar <acs-03@hotmail.com>
2022-01-04 21:39:10 -0300

    Refactor pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,17 +1,23 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ReactPaginate from 'react-paginate';
 
 import './styles.css';
 
 export default function Pagination() {
   return (
-    <div className="pagination-container">
-      <ArrowIcon className="arrow-previous arrow-inactive"/>
-      <div className="pagination-item active">1</div>
-      <div className="pagination-item">2</div>
-      <div className="pagination-item">3</div>
-      <div className="pagination-item">...</div>
-      <div className="pagination-item">10</div>
-      <ArrowIcon className="arrow-next arrow-active"/>
-    </div>
+    <ReactPaginate
+      pageCount={10}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      containerClassName="pagination-container"
+      pageLinkClassName="pagination-item"
+      breakClassName="pagination-item"
+      previousLabel={<ArrowIcon />}
+      nextLabel={<ArrowIcon />}
+      previousClassName="arrow-previous"
+      nextClassName="arrow-next"
+      activeLinkClassName="pagination-link-active"
+      disabledClassName="arrow-inactive"
+    />
   );
 }
