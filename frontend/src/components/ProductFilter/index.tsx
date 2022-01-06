afc653df72120db1ddf178cee1057e3ae13c2a2d
Augusto Cesar <acs-03@hotmail.com>
2022-01-06 16:41:52 -0300

    Basic HTML ProductFilter

--- frontend/src/components/ProductFilter/index.tsx
+++ frontend/src/components/ProductFilter/index.tsx
@@ -1,9 +1,23 @@
-import "./styles.css"
+import './styles.css';
 
 const ProductFilter = () => {
-    return(
-        <div className="base-card product-filter-container">Search bar</div>
-    )
-}
+  return (
+    <div className="base-card product-filter-container">
+      <form action="" className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input type="text" className="form-control" />
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select name="" id="">
+              <option value="">livros</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">limpar filtro</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ProductFilter;
