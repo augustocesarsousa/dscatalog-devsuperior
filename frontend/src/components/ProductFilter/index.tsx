58ba7fd342e013bbb956a71729f2f81b179ccc88
Augusto Cesar <acs-03@hotmail.com>
2022-01-06 16:57:59 -0300

    ProductFilter layout input name

--- frontend/src/components/ProductFilter/index.tsx
+++ frontend/src/components/ProductFilter/index.tsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg';
 
 const ProductFilter = () => {
   return (
     <div className="base-card product-filter-container">
       <form action="" className="product-filter-form">
         <div className="product-filter-name-container">
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Nome do produto"
+          />
+          <SearchIcon />
         </div>
         <div className="product-filter-bottom-container">
           <div className="product-filter-category-container">
