eb91adb0844f653fcf4daf244ffc737ed34990d1
Augusto Cesar <acs-03@hotmail.com>
2022-01-15 15:09:13 -0300

    Block describe

--- frontend/src/util/__tests__/formatters.spec.ts
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -1,12 +1,25 @@
 import { formatPrice } from 'util/formatters';
 
-test('formatPrice shoud format number pt-BR when given 10.1', () => {
-  //ARRANGE
-  const value = 10.1;
+describe('formatPrice for positive numbers', () => {
+  test('formatPrice shoud format number pt-BR when given 10.1', () => {
+    const result = formatPrice(10.1);
+    expect(result).toEqual('10.10');
+  });
 
-  //ACT
-  const result = formatPrice(value);
-
-  //ASSERT
-  expect(result).toEqual('10.10');
+  test('formatPrice shoud format number pt-BR when given 0.1', () => {
+    const result = formatPrice(0.1);
+    expect(result).toEqual('0.10');
+  });
 });
+
+describe('formatPrice for non-positive numbers', () => {
+    test('formatPrice shoud format number pt-BR when given 0', () => {
+      const result = formatPrice(0);
+      expect(result).toEqual('0.00');
+    });
+  
+    test('formatPrice shoud format number pt-BR when given -5.1', () => {
+      const result = formatPrice(-5.1);
+      expect(result).toEqual('-5.10');
+    });
+  });
