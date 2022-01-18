5b2e31a133307f61feac614502d60f3780c8e153
Augusto Cesar <acs-03@hotmail.com>
2022-01-17 21:49:17 -0300

    First test hasAnyRoles

---
+++ frontend/src/util/__tests__/auth.spec.ts
@@ -0,0 +1,8 @@
+import { hasAnyRoles } from '../auth';
+
+describe('hasAnyRoles tests', () => {
+  test('should return true when empty list', () => {
+    const result = hasAnyRoles([]);
+    expect(result).toEqual(true);
+  });
+});
