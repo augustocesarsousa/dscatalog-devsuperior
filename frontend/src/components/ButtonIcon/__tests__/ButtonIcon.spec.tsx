f7b657e3cc9cad0b6a6e674b74d34b9aa118b5f5
Augusto Cesar <acs-03@hotmail.com>
2022-01-17 23:35:07 -0300

    ButtonIcon test

---
+++ frontend/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import ButtoIcon from '..';
+
+test('Buttonicon should render button with given text', () => {
+  const text = 'Botão';
+
+  render(<ButtoIcon text={text} />);
+
+  expect(screen.getByText(text)).toBeInTheDocument();
+});
