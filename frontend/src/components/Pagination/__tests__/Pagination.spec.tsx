2d192345b63d0161bff03da8485e101380d23fc7
Augusto Cesar <acs-03@hotmail.com>
2022-01-18 22:30:37 -0300

    Pagination rander tests

---
+++ frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import Pagination from '..';
+
+describe('Pagination tests', () => {
+  test('should rander Pagination', () => {
+    const pageCount = 3;
+    const range = 3;
+
+    render(<Pagination pageCount={pageCount} range={range} />);
+
+    const page1 = screen.getByText('1');
+    const page2 = screen.getByText('2');
+    const page3 = screen.getByText('3');
+    const page4 = screen.queryByText('4');
+
+    expect(page1).toBeInTheDocument();
+    expect(page1).toHaveClass('pagination-link-active');
+
+    expect(page2).toBeInTheDocument();
+    expect(page2).not.toHaveClass('pagination-link-active');
+
+    expect(page3).toBeInTheDocument();
+    expect(page3).not.toHaveClass('pagination-link-active');
+
+    expect(page4).not.toBeInTheDocument();
+  });
+});
