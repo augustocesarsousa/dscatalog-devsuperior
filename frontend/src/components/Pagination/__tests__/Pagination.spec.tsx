27e82eb74da642a12dafff6399ab06db80e64988
Augusto Cesar <acs-03@hotmail.com>
2022-01-18 23:36:25 -0300

    Pagination change page test

--- frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -40,4 +40,40 @@ describe('Pagination tests', () => {
     userEvent.click(arrowNext);
     expect(onChange).toHaveBeenCalledWith(1);
   });
+
+  test('previous arrow should call onChange', () => {
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn();
+    const forcePage = 1;
+
+    render(
+      <Pagination
+        pageCount={pageCount}
+        range={range}
+        onChange={onChange}
+        forcePage={forcePage}
+      />
+    );
+
+    const arrowPrevious = screen.getByTestId('arrow-previous');
+
+    userEvent.click(arrowPrevious);
+    expect(onChange).toHaveBeenCalledWith(0);
+  });
+
+  test('page link should call onChange', () => {
+    const pageCount = 3;
+    const range = 3;
+    const onChange = jest.fn();
+
+    render(
+      <Pagination pageCount={pageCount} range={range} onChange={onChange} />
+    );
+
+    const page2 = screen.getByText('2');
+
+    userEvent.click(page2);
+    expect(onChange).toHaveBeenCalledWith(1);
+  });
 });
