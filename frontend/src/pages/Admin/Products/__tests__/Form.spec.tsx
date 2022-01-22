6409a5e06361ca899dda87ae4923a0780daa6739
Augusto Cesar <acs-03@hotmail.com>
2022-01-21 22:03:36 -0300

    Testing sucess toast

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Router, useParams } from 'react-router-dom';
 import selectEvent from 'react-select-event';
+import { ToastContainer } from 'react-toastify';
 import history from 'util/history';
 import Form from '../Form';
 import { server } from './fixtures';
@@ -16,33 +17,42 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('product form create tests', () => {
-    beforeEach(() => {
-        (useParams as jest.Mock).mockReturnValue({
-            productId: 'create'
-        })
-    })
+  beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+      productId: 'create',
+    });
+  });
 
   test('should render Form', async () => {
     render(
       <Router history={history}>
+        <ToastContainer />
         <Form />
       </Router>
     );
 
-    const nameInput = screen.getByTestId("name");
-    const priceInput = screen.getByTestId("price");
-    const imgUrlInput = screen.getByTestId("imgUrl");
-    const descriptionInput = screen.getByTestId("description");
-    const categoriesInput = screen.getByLabelText("Categorias");
-    const submitButton = screen.getByRole('button', { name: /salvar/i})
+    const nameInput = screen.getByTestId('name');
+    const priceInput = screen.getByTestId('price');
+    const imgUrlInput = screen.getByTestId('imgUrl');
+    const descriptionInput = screen.getByTestId('description');
+    const categoriesInput = screen.getByLabelText('Categorias');
+    const submitButton = screen.getByRole('button', { name: /salvar/i });
 
-    await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos'])
-    
+    await selectEvent.select(categoriesInput, ['Computadores', 'Eletrônicos']);
+
     userEvent.type(nameInput, 'Computador');
     userEvent.type(priceInput, '5000.12');
-    userEvent.type(imgUrlInput, 'https://i.kym-cdn.com/entries/icons/facebook/000/019/649/OK_thumb.jpg');
+    userEvent.type(
+      imgUrlInput,
+      'https://i.kym-cdn.com/entries/icons/facebook/000/019/649/OK_thumb.jpg'
+    );
     userEvent.type(descriptionInput, 'Computador i7, 8GB ram, 512 SSD');
 
     userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText('Produto cadastrado com sucesso!');
+      expect(toastElement).toBeInTheDocument();
+    });
   });
 });
