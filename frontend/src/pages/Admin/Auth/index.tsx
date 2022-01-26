8262e75b675ef8cdec7264af01176c46f2e8dd51
Augusto Cesar <acs-03@hotmail.com>
2022-01-26 16:40:53 -0300

    Refactor responsive layout

--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -1,30 +1,35 @@
-import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg'
-import { Switch, Route } from 'react-router-dom';
+import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
+import { Route, Switch } from 'react-router-dom';
 import Login from './Login';
-
 import './styles.css';
 
-export default function Auth () {
-    return(
-        <div className="auth-container">
-            <div className="auth-banner-container">
-                <h1>Divulgue seus produtos no DS Catalog</h1>
-                <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
-                <AuthImage />
-            </div>
-            <div className="auth-form-container">
-                <Switch>
-                    <Route path="/admin/auth/login">
-                        <Login />
-                    </Route>
-                    <Route path="/admin/auth/signup">
-                        <h1>Card Signup</h1>
-                    </Route>
-                    <Route path="/admin/auth/recover">
-                        <h1>Card Recover</h1>
-                    </Route>
-                </Switch>
-            </div>
+
+export default function Auth() {
+  return (
+    <div className="auth-container">
+      <div className="auth-content">
+        <div className="auth-banner-container">
+          <h1>Divulgue seus produtos no DS Catalog</h1>
+          <p>
+            Faça parte do nosso catálogo de divulgação e aumente a venda dos
+            seus produtos.
+          </p>
+          <AuthImage />
         </div>
-    );
+        <div className="auth-form-container">
+          <Switch>
+            <Route path="/admin/auth/login">
+              <Login />
+            </Route>
+            <Route path="/admin/auth/signup">
+              <h1>Card Signup</h1>
+            </Route>
+            <Route path="/admin/auth/recover">
+              <h1>Card Recover</h1>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-
 import './styles.css';
+
 
 const Home = () => {
   return (
