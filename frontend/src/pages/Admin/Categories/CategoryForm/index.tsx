14a16e9499beac2a55c0972434768e05bd117046
Augusto Cesar <acs-03@hotmail.com>
2022-02-07 18:08:51 -0300

    CategoryFormCard created

--- frontend/src/pages/Admin/Categories/CategoryForm/index.tsx
+++ frontend/src/pages/Admin/Categories/CategoryForm/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import "./styles.css";
 
 function CategoryForm() {
   return (
-    <div>
-      <h1>Category Form</h1>
+    <div className="category-form-card-container base-card">
+      <form className="category-form-card-content">
+        <div className="category-form-card-input-container">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Categoria"
+          />
+        </div>
+        <div className="category-form-card-buttons-container">
+          <button className="btn btn-primary category-form-card-button">
+            salvar
+          </button>
+          <button className="btn btn-outline-danger category-form-card-button">
+            cancelar
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
